e9e3000fd4c958cb4e10e15fd3f77ec4ef4e0573
Chris Dalzell <cdalzell@gmail.com>
2017-01-06 11:40:15 -0500

    Removing older code that stuck around post merge

--- src/main/scala/treeinterpreter/shadowforest/LeafNode.scala
+++
@@ -1,7 +0,0 @@
-package treeinterpreter.shadowforest
-
-case class LeafNode(shadowNode: ShadowNode, steps: Array[TraversalStep]) {
-  def impurityStats: Array[Double] = {
-    shadowNode.impurityStats
-  }
-}
